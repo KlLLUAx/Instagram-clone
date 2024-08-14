31df842b0d8a282caa7c72615a0001f2a02e04ec
killua <killualolxx@gmail.com>
2024-08-14 09:24:17 -0300

    finish login and password input styling and transnform effect

--- scripts/loginScreen.js
+++ scripts/loginScreen.js
@@ -1,10 +1,19 @@
 const loginInput = document.querySelector('.js-login-input');
 const passwordInput = document.querySelector('.js-password-input');
+
 loginInput.addEventListener('input',()=>{
   if(loginInput.value){
-    document.querySelector('.js-test').classList.add('upper-placeholder-on')
+    document.querySelector('.js-login').classList.add('upper-placeholder-on')
     return
   }
-  document.querySelector('.js-test').classList.remove('upper-placeholder-on')
+  document.querySelector('.js-login').classList.remove('upper-placeholder-on');
+})
+
+passwordInput.addEventListener('input',()=>{
+  if(passwordInput.value){
+    document.querySelector('.js-password').classList.add('upper-placeholder-on');
+    return
+  }
+  document.querySelector('.js-password').classList.remove('upper-placeholder-on');
 
 })
